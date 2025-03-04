e6707fb3925f3a17a4dc772fb4a7be6c13031350
like0617 <381170854@qq.com>
2025-03-04 08:17:42 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '1339d12e-327f-4dc7-b8bf-c0e603de158f';
+let userID = '6d26507b-5cf3-4cfe-a3b6-62d951bee4ef';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
